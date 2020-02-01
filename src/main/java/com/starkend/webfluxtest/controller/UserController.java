b4283c300e4602fdaf085d01927037331f6e07f4
starkend <danstarken@gmail.com>
2020-02-01 10:13:30 -0600

    Refactoring and code cleanup.

--- src/main/java/com/starkend/webfluxtest/controller/UserController.java
+++ src/main/java/com/starkend/webfluxtest/controller/UserController.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @RequestMapping("wf")
 public class UserController {
 
-    private Logger logger = LoggerFactory.getLogger(UserController.class);
+    private Logger LOG = LoggerFactory.getLogger(UserController.class);
     private UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
@@ -26,7 +26,9 @@ public class UserController {
 
     @GetMapping("/users")
     public Flux<User> getAllUsers() {
-        return userRepository.findAll();
+        Flux<User> userFlux = userRepository.findAll();
+
+        return userFlux;
     }
 
     @GetMapping("/saveUser")
