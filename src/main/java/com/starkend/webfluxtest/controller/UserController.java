2598c4ad6af9af79c2a279eb1f1af085acfdab41
starkend <danstarken@gmail.com>
2020-02-29 10:59:33 -0600

    Updated Controller and Service classes to remove direct Repository calls

--- src/main/java/com/starkend/webfluxtest/controller/UserController.java
+++ src/main/java/com/starkend/webfluxtest/controller/UserController.java
@@ -12,56 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.UUID;
-
 @RestController
 @RequestMapping("wf")
 public class UserController {
 
     private Logger LOG = LoggerFactory.getLogger(UserController.class);
-    private UserRepository userRepository;
     private UserService userService;
 
     public UserController(UserRepository userRepository, UserService userService) {
-        this.userRepository = userRepository;
         this.userService = userService;
     }
 
     @GetMapping("/users")
     public Flux<User> getAllUsers() {
-        Flux<User> userFlux = userService.findAllUsers();
-
-        return userFlux;
+        return userService.findAllUsers();
     }
 
     @GetMapping("/saveUser")
     public Mono<User> saveUser(@RequestParam String userName) {
-        Mono<User> saveUser = userRepository.save(new User(UUID.randomUUID().toString(), userName));
-
-        return saveUser;
+        return userService.saveUser(userName);
     }
 
     @GetMapping("/findUser")
     public Flux<User> findUser(@RequestParam String userName) {
-        Flux<User> userFlux = userRepository.findByName(userName);
-
-        return userFlux;
+        return userService.findUser(userName);
     }
 
     @GetMapping("/findUserByPartialName")
     public Flux<User> findUserByPartialName(@RequestParam String userName) {
-        Flux<User> userFlux = userRepository.findByNameContaining(userName);
-
-        return userFlux;
+        return userService.findUserByPartialName(userName);
     }
 
     @GetMapping("/userExists")
     public Mono<Boolean> userExists(@RequestParam String userId) {
-        return userRepository.existsById(userId);
+        return userService.userExists(userId);
     }
 
     @GetMapping("userCount")
     public Mono<Long> userCount() {
-        return userRepository.count();
+        return userService.userCount();
     }
 }
--- src/main/java/com/starkend/webfluxtest/service/UserService.java
+++ src/main/java/com/starkend/webfluxtest/service/UserService.java
@@ -3,7 +3,12 @@ package com.starkend.webfluxtest.service;
 import com.starkend.webfluxtest.model.User;
 import com.starkend.webfluxtest.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -17,4 +22,25 @@ public class UserService {
     public Flux<User> findAllUsers() {
         return userRepository.findAll();
     }
+
+    public Mono<User> saveUser(String userName) {
+        return userRepository.save(new User(UUID.randomUUID().toString(), userName));
+    }
+
+    public Flux<User> findUser(String userName) {
+        return userRepository.findByName(userName);
+    }
+
+    public Flux<User> findUserByPartialName(String userName) {
+        return userRepository.findByNameContaining(userName);
+    }
+
+    public Mono<Boolean> userExists(String userId) {
+        return userRepository.existsById(userId);
+    }
+
+    public Mono<Long> userCount() {
+        return userRepository.count();
+    }
+
 }
