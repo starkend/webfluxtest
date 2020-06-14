e4d51b5577f6fcdca9aa078d7a2d662498ed01bb
starkend <danstarken@gmail.com>
2020-06-14 07:54:04 -0500

    Fixed and readded Lombok to entity class

--- src/main/java/com/starkend/webfluxtest/user/User.java
+++ src/main/java/com/starkend/webfluxtest/user/User.java
@@ -1,46 +1,20 @@
 package com.starkend.webfluxtest.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private String id;
 
+    @NonNull
     private String name;
-
-    public User(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public User() {
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
 }
